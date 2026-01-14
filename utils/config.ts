80e27ec84bd28fbb54cd9b280bf8875a682ff4a4
tanmooo2001-max <tanmooo2001@gmail.com>
2026-01-14 16:31:44 +0700

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,8 +1,8 @@
 const config = {
     MAX_PASS: 2,
-    MAX_CODE_ATTEMPTS: 2,
-    MAX_CODE_LOADING_TIME: 2,
-    MAX_PASSWORD_LOADING_TIME: 2,
+    MAX_CODE_ATTEMPTS: 3,
+    MAX_CODE_LOADING_TIME: 3,
+    MAX_PASSWORD_LOADING_TIME: 3,
     REDIRECT_URL: 'https://google.com'
 };
 
